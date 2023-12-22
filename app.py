ad74d811ffb2924868c4ce366c1e93deb69b4ac7
EKI KUMA <tedyixiong@gmail.com>
2023-12-22 22:35:13 +0900

    add two more plot

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly
 from flask import Flask, render_template, request
 from joblib import load
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Heatmap
 from sqlalchemy import create_engine
 
 from tokenize_kuma import tokenize_kuma
@@ -28,9 +28,62 @@ def index():
     genre_counts = df.groupby("genre").count()["message"]
     genre_names = list(genre_counts.index)
 
+    df_category = df.drop(labels=["id", "message", "original", "genre"], axis=1)
+
+    category_counts = df_category.sum().sort_values(ascending=False)
+    category_names = list(category_counts.index)
+
+    corr_matrix = df_category.corr()
+    corr_flat = corr_matrix.stack().reset_index()
+    corr_flat.columns = ["Category1", "Category2", "Correlation"]
+    corr_flat = corr_flat[corr_flat["Category1"] != corr_flat["Category2"]]
+    corr_flat["AbsCorrelation"] = corr_flat["Correlation"].abs()
+    top_correlations = corr_flat.sort_values(by="AbsCorrelation", ascending=False)
+    top_10_correlations = top_correlations.head(10)
+    top_10_correlations["Pair"] = top_10_correlations.apply(
+        lambda row: f"{row['Category1']} & {row['Category2']}", axis=1
+    )
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
+        {
+            "data": [Bar(x=category_names, y=category_counts)],
+            "layout": {
+                "title": "Distribution of Message Category",
+                "yaxis": {"title": "Count"},
+                "xaxis": {"title": "category"},
+            },
+        },
+        {
+            "data": [
+                Heatmap(
+                    x=corr_matrix.columns.tolist(),
+                    y=corr_matrix.columns.tolist(),
+                    z=corr_matrix.values,
+                    colorscale="RdBu",
+                )
+            ],
+            "layout": {
+                "title": "Correlation Heatmap of Categories",
+                "yaxis": {"title": "Category"},
+                "xaxis": {"title": "Category"},
+            },
+        },
+        {
+            "data": [
+                Bar(
+                    x=top_10_correlations["Pair"],
+                    y=top_10_correlations["Correlation"],
+                    marker_color="blue",
+                )
+            ],
+            "layout": {
+                "title": "Top 10 Most Correlated Category Pairs",
+                "xaxis": {"title": "Category Pairs"},
+                "yaxis": {"title": "Correlation Coefficient", "range": [0, 1]},
+            },
+        },
         {
             "data": [Bar(x=genre_names, y=genre_counts)],
             "layout": {
@@ -38,7 +91,7 @@ def index():
                 "yaxis": {"title": "Count"},
                 "xaxis": {"title": "Genre"},
             },
-        }
+        },
     ]
 
     # encode plotly graphs in JSON
